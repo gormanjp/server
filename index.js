b9a16baf4e6d1ff40c9f881449ed8d86280ce39c
gormanjp <jpgorman8@gmail.com>
2018-01-22 13:26:46 -0500

    add signup route

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const router = require("./router");
 const app = express();
+
+// DB setup
+mongoose.connect("mongodb://localhost/auth");
 
 // App Setup
 // app.use sends all requests through middleware (morgan and bodyParser)
